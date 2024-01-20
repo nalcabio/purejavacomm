310abfd13c8614c0b0776d4b306f0ba4030ae299
Tom Vaughan <thomas.david.vaughan@gmail.com>
2024-01-20 14:53:56 -0300

    Bump net.java.dev.jna and release v1.0.4.

--- src/purejavacomm/PureJavaComm.java
+++ src/purejavacomm/PureJavaComm.java
@@ -3,7 +3,7 @@ package purejavacomm;
 public class PureJavaComm {
 	// change this if you modify the code and re-distribute
 	public static String getVersion() {
-		return "1.0.3";
+		return "1.0.4";
 	}
 
 	// change this is if you fork the project
